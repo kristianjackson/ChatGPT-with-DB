8c8e3d9bd06cb4e51d723488b571907e51fedb6a
kristianjackson <kristian.jackson@gmail.com>
2023-04-12 14:55:00 -0400

    updated tab title

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorsto
 
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="Blendle Notion QA Bot", page_icon=":robot:")
+st.set_page_config(page_title="Kristian's Custom ChatGPT", page_icon=":robot:")
 st.header("Kristian's Custom ChatGPT")
 
 if "generated" not in st.session_state:
